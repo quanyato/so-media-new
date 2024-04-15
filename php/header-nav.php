868d9dfda15b7eb48a9f0f6d5f3454fe409608f6
quanyato <quanlb.b21cn607@stu.ptit.edu.vn>
2024-04-16 02:24:44 +0700

    use php instead of html

---
+++ php/header-nav.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-md sticky-top py-3 navbar-dark" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="/">
+            <img src="./assets/img/logo-white.png" height="22px" class="me-2" alt="LOGO">
+            <span>Sổ Media</span>
+        </a>
+        <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
+            <span class="visually-hidden">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="./index.php#hero">Trang chủ</a></li>
+                <li class="nav-item"><a class="nav-link" href="./index.php#about">Giới thiệu</a></li>
+                <li class="nav-item"><a class="nav-link" href="./index.php#department">Các ban</a></li>
+                <li class="nav-item"><a class="nav-link" href="./index.php#training">Đào tạo</a></li>
+                <li class="nav-item"><a class="nav-link" href="./index.php#testimonials">Nhận xét</a></li>
+                <li class="nav-item"><a class="nav-link" href="./index.php#contact">Liên hệ</a></li>
+                <li class="nav-item"><a class="nav-link" href="./projects.php">Dự án</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ projects.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="vi">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./assets/img/logo-white.png" type="image/x-icon" media="(prefers-color-scheme: dark)">
+    <link rel="shortcut icon" href="./assets/img/logo-black.png" type="image/x-icon" media="(prefers-color-scheme: light)">
+
+    <title>Dự án - Sổ Media</title>
+
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css?h=4a137410e50fe91a22b09a0763454a44">
+    <link rel="stylesheet" href="./assets/css/styles.min.css?h=ade9f8059fcfdd33307dee5e5390c9a0">
+    <link rel="stylesheet" href="./assets/css/custom.css">
+</head>
+
+<body>
+    <?php include './php/header.php'; ?>
+
+    <section class="py-5">
+        <div class="container py-5">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h2 class="fw-bold">Các dự án</h2>
+                    <p class="text-muted">Một số dự án nổi bật của Gia đình nhà Sổ</p>
+                </div>
+            </div>
+            <div id="projectList" class="row row-cols-1 row-cols-md-2 mx-auto" style="max-width: 900px;">
+                <div id="project1" class="col mb-4">
+                    <div><a href="#"><img class="rounded img-fluid shadow w-100 fit-cover" src="assets/img/products/1.jpg?h=fc1e51d44f18accfa3a90f81c9cfffe4" style="height: 250px;"></a>
+                        <div class="py-4"><span class="badge bg-primary mb-2">Website</span>
+                            <h4 class="fw-bold">Lorem libero donec</h4>
+                            <p class="text-muted">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include './php/footer.php'; ?>
+</body>
+
+</html>
